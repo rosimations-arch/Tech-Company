bc7ac0679ae063d5a8dbced920ede7152dd03c05
rosimations-arch <rosimations@gmail.com>
2026-05-27 22:08:54 +0800

    Add stub email_parser for integrators

---
+++ integrators/email_parser.py
@@ -0,0 +1,4 @@
+# Placeholder: Here you could use libraries like imaplib/email to fetch & parse invoices from email.
+# Requires user setup for their own email account.
+# This is just a stub to get you started.
+print("Email parser would go here.")
